cde3a3a863a97ae930c69cb92ab440182a942c01
pcooney10 <pcooney9@fordham.edu>
2015-10-20 17:03:24 -0400

    added some jade ish

--- src/app.js
+++ src/app.js
@@ -5,21 +5,24 @@ var posts = require('./mock/posts.json');
 
 var app = express();
 
+app.set('view engine', 'jade'); //`app.set` defines different settings in express
+app.set('views', __dirname + '/templates');
+
 app.get('/', function(req, res) {
-	res.send("<h1>I am loving Treehouse!</h1><br><h1>Until I have to pay for it!</h1>");
+	res.render('index') // res.render('index.jade') not necessary bc we set engine to jade
 });
 
-app.get('/blog/:title', function(req, res) {
+app.get('/blog/:title?', function(req, res) {
 	// makes the param the id: /blog/:id
 	// makes the param optional: /blog/:title?
 	// `debugger;` --> tells node-inspector to stop here.  typing `res` in the console will allow you to see the entire response object
 	var title = req.params.title;
-	if (title ==== undefined) {
+	if (title === undefined) {
 			res.status(503);
 			res.send("This page is under construction. My b.");
 	}	else {
-			var post = posts[title];
-			res.send(post);
+			var post = posts[title] || {};
+			res.render('post', { post: post});
 	}
 });
 
