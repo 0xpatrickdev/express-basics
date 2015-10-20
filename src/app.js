b0673715f5e2eae66813597f1fbe9b8258adbb92
pcooney10 <pcooney9@fordham.edu>
2015-10-20 17:04:35 -0400

    deleting comments, but refer to the change log for notes.. you took the time to write them, so plz read them over

--- src/app.js
+++ src/app.js
@@ -5,17 +5,14 @@ var posts = require('./mock/posts.json');
 
 var app = express();
 
-app.set('view engine', 'jade'); //`app.set` defines different settings in express
+app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res) {
-	res.render('index') // res.render('index.jade') not necessary bc we set engine to jade
+	res.render('index');
 });
 
 app.get('/blog/:title?', function(req, res) {
-	// makes the param the id: /blog/:id
-	// makes the param optional: /blog/:title?
-	// `debugger;` --> tells node-inspector to stop here.  typing `res` in the console will allow you to see the entire response object
 	var title = req.params.title;
 	if (title === undefined) {
 			res.status(503);
